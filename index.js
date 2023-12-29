55a5bbde3f22c485fd8f2d5e0362a364e5170a53
Manny <manuelpo.msn.com>
2023-12-28 19:41:47 -0700

    all done

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const main = document.querySelector('main#main');
 main.remove();
 
 //create new element <h1>
-const newHeader = document.createElement('h1');
+var newHeader = document.createElement('h1');
 
 // Set the id attribute of the new <h1> element to 'victory'
-newHeader.id = 'victory';
+newHeader.id = "victory";
 
 // Set the text content of the new <h1> element to "YOUR-NAME is the champion"
 
-newHeader.textContent = 'Manuel is the champion';
+newHeader.textContent = "Manuel is the champion";
 
 // Append the new <h1> element to the <body>
 document.body.appendChild(newHeader);
